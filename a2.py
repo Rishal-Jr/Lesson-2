46f18374cc89ca6fe90188bf9a716e99503ca2bc
Rishal-Jr <rmrazni@gmail.com>
2024-12-17 18:48:31 +0530

    hh

---
+++ a2.py
@@ -0,0 +1,6 @@
+x=5
+b=7
+print(x)
+print(b)
+name=input("enter your name:")
+print("\nHello",name,"Welcome to codingal")
---
+++ a3.py
@@ -0,0 +1,3 @@
+import keyword
+print("The keywords of python are...\n")
+print(keyword.kwlist)
